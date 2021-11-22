bb9b16fa7db88517c97436677a9a31f7cb68bdba
leejuyun <godwjdch63@naver.com>
2021-11-22 15:00:04 +0900

    create_smtp

---
+++ src/mail/SMTPAuthenticator.java
@@ -0,0 +1,11 @@
+package mail;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+
+public class SMTPAuthenticator extends Authenticator{
+	@Override
+	protected PasswordAuthentication getPasswordAuthentication(){
+		return new PasswordAuthentication("包府磊 皋老","包府磊 皋老 pw");
+	}
+}
